318db23332311d5d6da0fd1f4ec6c0af564dd84b
Alice Pereira <alicecavalcanti24@gmail.com>
2024-11-25 16:45:27 -0300

    (refactor) : remove unnecessary properties

--- src/main/kotlin/com/challenge/library/controller/dto/LateAndOnTimeReturnsDTO.kt
+++ src/main/kotlin/com/challenge/library/controller/dto/LateAndOnTimeReturnsDTO.kt
@@ -1,7 +1,6 @@
 package com.challenge.library.controller.dto
 
 data class LateAndOnTimeReturnsDTO (
-    val idBook: String,
     val loanOnTime: Int,
-    val lateLoan:Int
+    val lateLoan: Int
 )
