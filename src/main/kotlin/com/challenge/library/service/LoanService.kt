282c1a96b6a2ee769dd2640d3539445a02add57d
Alice Pereira <alicecavalcanti24@gmail.com>
2024-11-25 17:00:01 -0300

    (refactor) : simplify and optimize methods

--- src/main/kotlin/com/challenge/library/service/LoanService.kt
+++ src/main/kotlin/com/challenge/library/service/LoanService.kt
@@ -4,7 +4,6 @@ import com.challenge.library.controller.dto.*
 import com.challenge.library.exception.*
 import com.challenge.library.mapper.LoanRequestMapper
 import com.challenge.library.model.Loan
-import com.challenge.library.model.LoanStatus
 import com.challenge.library.repository.LoanRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -30,91 +29,73 @@ class LoanService (
         pagination: Pageable
     ): Page<Loan>? {
         val user = userService.findUserById(idUser)
-
-        val loanAndReturn = user.id?.let { loanRepository.findAllByIdUser(it, pagination) }
-        return loanAndReturn
+        return loanRepository.findAllByIdUser(user.id!!, pagination)
     }
 
-    fun findLoanById(idLoan: String): Loan{
+    fun findLoanById(idLoan: String): Loan {
         return loanRepository.findById(idLoan).orElseThrow{ LoanNotFoundException() }
     }
 
+
     fun registerLoad(loanBookDTO: LoanBookDTO) : Loan {
         val book = bookService.findBookById(loanBookDTO.idBook)
-
         val listLoansFound = loanRepository.findAllByIdBook(book.id)
-
-        listLoansFound.any { it.isLoanReturned() } .run { throw BookAlreadyRequestedException() }
-
+        if(listLoansFound.any{ it.isLoanNotReturned()}) throw BookAlreadyRequestedException()
         val loanCreate= loanRequestMapper.map(loanBookDTO)
-
-        val createLoan= loanRepository.save(loanCreate)
-
-        return createLoan
+        return loanRepository.save(loanCreate)
     }
 
     fun approveLoanRequest(idLoan: String): Loan{
-        var loanFound= findLoanById(idLoan)
-
-        loanFound.checkIfStatusNotValid(LoanStatus.REQUESTED_LOAN)
-
-        loanFound.status= LoanStatus.APPROVED
-        this.loanRepository.save(loanFound)
-
-        return loanFound
-
+        val loanFound = findLoanById(idLoan)
+        if (!loanFound.approveLoan()) throw NotApprovedLoanException()
+        return loanRepository.save(loanFound)
     }
 
     fun approveReturnRequest(idLoan: String): Loan {
-        var loanFound= findLoanById(idLoan)
-
-        loanFound.checkIfStatusNotValid(LoanStatus.REQUESTED_RETURN )
-
-        loanFound.status= LoanStatus.RETURNED
-        this.loanRepository.save(loanFound)
-
-        return loanFound
+        val loanFound= findLoanById(idLoan)
+        if (!loanFound.approveReturn()) throw NotRequestedReturnLoanException()
+        return loanRepository.save(loanFound)
     }
 
-    fun grabBook(idLoan: String) : Loan{
+    fun grabLoanBook(idLoan: String) : Loan{
         val loanFound= findLoanById(idLoan)
-        loanFound.checkIfStatusNotValid(LoanStatus.APPROVED)
-
-        loanFound.status = LoanStatus.CHECKED_OUT
+        if (!loanFound.grabBook()) throw NotCheckedOutLoanException()
         loanRepository.save(loanFound)
-
         return loanFound
-
     }
 
     fun bookReturn(
         idLoan: String
     ): Loan{
         val loanFound = findLoanById(idLoan)
-        loanFound.checkIfStatusNotValid(LoanStatus.CHECKED_OUT)
-
-        loanFound.status = LoanStatus.REQUESTED_RETURN
-        loanFound.userReturnDate = LocalDate.now()
-        loanRepository.save(loanFound)
-
-        return loanFound
+        if (!loanFound.requestbookReturn()) throw NotRequestedReturnLoanException()
+        return loanRepository.save(loanFound)
     }
+
     fun findAllLoanMonth(): List<Loan>{
         val end = LocalDate.now().atTime(LocalTime.MAX)
         val start = LocalDate.now().atStartOfDay().minusMonths(1)
         return loanRepository.findLoansByPeriod(start, end)
     }
 
-    fun fiveMostBorrowedBooks(): List<FiveMostBorrowedBooksDTO>{
-        return loanRepository.fiveMostBorrowedBooks()
+    fun fiveMostBorrowedBooks(): List<ThreeMostBorrowedBooksDTO>{
+        return loanRepository.threeMostBorrowedBooks()
     }
 
     fun lateAndOnTimeReturns(): List<LateAndOnTimeReturnsDTO>{
-        return loanRepository.findLateAndOnTimeReturns()
-    }
-    fun amountLoansCategory(): List<LoanAmountCategoryDTO>{
-        return loanRepository.findAmountLoansCategory()
-    }
+        val loans = loanRepository.findAllLoansReturns()
+        var loanOnTime = 0
+        var lateLoan = 0
 
+        loans.map { if(it.userReturnDate!! <= it.expectedReturnDate) loanOnTime += 1}
+        loans.map { if(it.userReturnDate!! > it.expectedReturnDate) lateLoan += 1}
+
+        return listOf (
+            LateAndOnTimeReturnsDTO(
+                loanOnTime = loanOnTime,
+                lateLoan = lateLoan
+            )
+        )
+    }
 
 }
