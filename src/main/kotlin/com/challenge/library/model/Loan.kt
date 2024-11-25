009c1214da84e1ca007cf4cf5a2cb80c43ca10ce
Alice Pereira <alicecavalcanti24@gmail.com>
2024-11-25 16:51:12 -0300

    (feat) : add and modify methods

--- src/main/kotlin/com/challenge/library/model/Loan.kt
+++ src/main/kotlin/com/challenge/library/model/Loan.kt
@@ -1,6 +1,5 @@
 package com.challenge.library.model
 
-import com.challenge.library.exception.*
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDate
@@ -8,36 +7,48 @@ import java.time.LocalDate
 @Document(value = "loan")
 class Loan(
     @Id
-    val id: String?= null,
+    var id: String?= null,
     val idUser: String,
     val idBook: String,
-    var status: LoanStatus,
+    var status: LoanStatus = LoanStatus.CHECKED_OUT,
+    var loanDate: LocalDate?=null,
     var expectedReturnDate: LocalDate ?= null,
     var userReturnDate:LocalDate?=null
-){
-
-    fun checkIfStatusNotValid(expectedStatus: LoanStatus): Boolean{
-        if(this.status != expectedStatus){
-            if(expectedStatus == LoanStatus.RETURNED) throw BookAlreadyRequestedException()
-            if(expectedStatus == LoanStatus.REQUESTED_LOAN) throw StatusRequestedLoanException()
-            if(expectedStatus == LoanStatus.REQUESTED_RETURN) throw StatusRequestedReturnException()
-            if(expectedStatus ==  LoanStatus.APPROVED) throw StatusApprovedException()
-            if(expectedStatus == LoanStatus.CHECKED_OUT) throw StatusCheckedOutException()
-        }
-        return true
-
-    }
-
+)  {
     //TODO: modificar, aqui precisa validar se o empréstimo é diferente de returned, porque se for ele não pode ser solicitado
-    fun isLoanReturned(): Boolean {
-        return this.status == LoanStatus.RETURNED
+    fun isLoanNotReturned(): Boolean {
+        return this.status != LoanStatus.RETURNED
     }
 
-
-    fun approveLoan(){
-        if(this.status != LoanStatus.REQUESTED_LOAN){
-            throw StatusRequestedLoanException()
-        }
+    fun approveLoan(): Boolean{
+        if (this.status != LoanStatus.REQUESTED_LOAN) return false
         this.status = LoanStatus.APPROVED
+        return true
+    }
+
+    fun approveReturn(): Boolean{
+        if(this.status != LoanStatus.REQUESTED_RETURN) return false
+        this.status= LoanStatus.RETURNED
+        return true
+    }
+
+    fun grabBook(): Boolean{
+        if(this.status != LoanStatus.APPROVED) return false
+        this.status = LoanStatus.CHECKED_OUT
+        return true
+    }
+
+    fun requestbookReturn(): Boolean{
+        if (this.status != LoanStatus.CHECKED_OUT) return false
+        this.status = LoanStatus.REQUESTED_RETURN
+        this.userReturnDate = LocalDate.now()
+        return true
+    }
+
+    fun isReturnOnTime(dataRetorno: LocalDate): Boolean{
+        if(dataRetorno.isBefore(this.expectedReturnDate) || dataRetorno.isEqual(this.expectedReturnDate)) {
+            return true
+        }
+        return false
     }
 }
