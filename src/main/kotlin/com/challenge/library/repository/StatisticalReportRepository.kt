95443735a461a12b9fa5443728b8b95726d722ea
Alice Pereira <alicecavalcanti24@gmail.com>
2024-11-26 14:12:13 -0300

    (refactor) : remove files

--- src/main/kotlin/com/challenge/library/repository/StatisticalReportRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.challenge.library.repository
-
-import com.challenge.library.model.StatisticalReport
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface StatisticalReportRepository: MongoRepository<StatisticalReport, String>
